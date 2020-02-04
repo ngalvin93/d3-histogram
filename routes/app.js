f31112acab6c033a13480c45e0e041c026cdde6a
Alvin Ng <ngalvin93@gmail.com>
2020-02-03 23:20:37 -0600

    pug init

--- routes/app.js
+++ routes/app.js
@@ -3,6 +3,11 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/html', 'index.html')))
+app.set('views', path.join(__dirname, '..', 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('home'))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
