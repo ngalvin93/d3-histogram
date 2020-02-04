3fbf14e098d46c5f5ab602ceb2f4b40b97a881c4
Alvin Ng <ngalvin93@gmail.com>
2020-02-03 23:28:30 -0600

    script tag

--- routes/app.js
+++ routes/app.js
@@ -6,7 +6,7 @@ const port = 3000
 app.set('views', path.join(__dirname, '..', 'views'))
 app.set('view engine', 'pug')
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + 'public'))
 
 app.get('/', (req, res) => res.render('home'))
 
