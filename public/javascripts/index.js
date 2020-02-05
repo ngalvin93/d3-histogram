d71bae1605949daf5992208811330c1570860332
Alvin Ng <ngalvin93@gmail.com>
2020-02-04 23:01:57 -0600

    d3 connected

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,14 @@
-console.log('connected')
+console.log('connected!!!')
+
+var data = [30, 86, 168, 281, 303, 365]
+
+d3.select('.greeting').style('color','red')
+    .text('using the text method')
+
+d3.select(".svg")
+.selectAll("div")
+.data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return d * 2 + "px"; })
+    .text(function(d) { return '$ ' + d; });
