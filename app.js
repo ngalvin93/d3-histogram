7964b5f0f4d9581dc3a9836d3c5ad17f4e50f064
Alvin Ng <ngalvin93@gmail.com>
2020-02-04 21:03:28 -0600

    script to html

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.set('views', path.join(__dirname, '..', 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('home'))
 
