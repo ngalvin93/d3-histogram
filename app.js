3ad67650e3ad6d2793acd854d8feae7810ad445b
Alvin Ng <ngalvin93@gmail.com>
2020-02-05 23:24:46 -0600

    fetch csv

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const data = require('./public/MOCK_DATA.json')
 const app = express()
 const port = 3000
 
@@ -9,5 +10,7 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('home'))
+
+app.get('/data', (req, res) => res.json(data))
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
