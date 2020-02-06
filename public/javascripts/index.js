0751767c32cee8a653d9b9da75b3ab474e386d6d
Alvin Ng <ngalvin93@gmail.com>
2020-02-05 22:46:34 -0600

    chart

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,39 @@
-console.log('connected!!!')
+console.log('connected!')
 
-var data = [30, 86, 168, 281, 303, 365]
+const data = [4, 8, 15, 16, 23, 42]
 
-d3.select('.greeting').style('color','red')
-    .text('using the text method')
+const width = 420,
+    barHeight = 20
 
-d3.select(".svg")
-.selectAll("div")
-.data(data)
-    .enter()
-    .append("div")
-    .style("width", function(d) { return d * 2 + "px"; })
-    .text(function(d) { return '$ ' + d; });
+// domain is the min and max values in data
+// range is the desired width of the chart
+const scale = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, width])
+
+const chart = d3.select('.chart')
+    .attr('width', width)
+    .attr('height', barHeight * data.length)
+
+const bar = chart.selectAll('g')
+    .data(data)
+.enter().append('g')
+    .attr('transform', (d, i) => 'translate(0,' + i * barHeight + ')')
+
+bar.append('rect')
+    .attr('width', scale)
+    .attr('height', barHeight - 1)
+
+bar.append('text')
+    .attr('x', d => scale(d) - 3)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.35em')
+    .text(d => d)
+
+// d3.select('.chart')
+//         .selectAll('div')
+//         .data(data)
+//     .enter().append('div')
+//         .style('width', d => scale(d) + 'px')
+//         .text(d => d)
+
