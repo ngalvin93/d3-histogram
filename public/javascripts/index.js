2bbd8b10d9e6689ba609e35c141838d1c55df522
Alvin Ng <ngalvin93@gmail.com>
2020-02-08 23:31:35 -0600

    chart axis

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -55,22 +55,54 @@ console.log('connected!!!!')
 //         console.log(histogram)
 //     })
 
-const x = d3.scaleLinear()
-    .domain([0, 100]) // min and the max of the data: 0 to 100 if percentages
-    .range([0, 400]) // the corresponding value I want in Pixel
+// const x = d3.scaleLinear()
+//     .domain([0, 100]) // min and the max of the data: 0 to 100 if percentages
+//     .range([0, 400]) // the corresponding value I want in Pixel
 
-const svg = d3.select("#dataviz_area")
-svg.append("circle")
-  .attr("cx", 0).attr("cy", 100).attr("r", 40).style("fill", "blue");
-svg.append("circle")
-  .attr("cx", x(25)).attr("cy", 100).attr("r", 40).style("fill", "red");
-svg.append("circle")
-  .attr("cx", x(75)).attr("cy", 100).attr("r", 40).style("fill", "green");
+// const circle = d3.select("#dataviz_area")
+// circle.append("circle")
+//   .attr("cx", 0).attr("cy", 100).attr("r", 40).style("fill", "blue");
+// circle.append("circle")
+//   .attr("cx", x(25)).attr("cy", 100).attr("r", 40).style("fill", "red");
+// circle.append("circle")
+//   .attr("cx", x(75)).attr("cy", 100).attr("r", 40).style("fill", "green");
 
-svg.call(d3.axisBottom(x))
+// circle.call(d3.axisBottom(x))
 
-console.log(x(0))
-console.log(x(25))
-console.log(x(50))
-console.log(x(75))
-console.log(x(100))
+// console.log(x(0))
+// console.log(x(25))
+// console.log(x(50))
+// console.log(x(75))
+// console.log(x(100))
+
+var margin = {top: 10, right: 40, bottom: 30, left: 30},
+    width = 450 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var sVg = d3.select("#Area")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  // translate this svg element to leave some margin.
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// X scale and Axis
+var x = d3.scaleLinear()
+    .domain([0, 100])         // This is the min and the max of the data: 0 to 100 if percentages
+    .range([0, width]);       // This is the corresponding value I want in Pixel
+sVg
+  .append('g')
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+
+// X scale and Axis
+var y = d3.scaleLinear()
+    .domain([0, 100])         // This is the min and the max of the data: 0 to 100 if percentages
+    .range([height, 0]);       // This is the corresponding value I want in Pixel
+sVg
+  .append('g')
+  .call(d3.axisLeft(y));
+
+
